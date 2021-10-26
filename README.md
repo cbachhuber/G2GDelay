b869000cd1756977dc02c54368293f1c15ad1f5a
Joshua Glazer <vashmata@gmail.com>
2021-10-26 21:44:25 +0200

    python3 histogram (#5)

--- README.md
+++ README.md
@@ -2,21 +2,18 @@
 
 ![Build](https://github.com/cbachhuber/G2GDelay/actions/workflows/build_arduino_code.yml/badge.svg)
 
-This repository contains all the information and software you need to build your own Glass-to-Glass (G2G) delay measurement system.
-In addition to software, you need the hardware detailed in the [Construction Manual](#construction-manual) below.
+This repository contains all the information and software you need to build your own Glass-to-Glass delay measurement system. You will need the hardware detailed in the [Construction Manual](#construction-manual) below. After that, follow the [software setup](#software-setup) and [hardware setup](#hardware-setup) and you'll be good to go to [take some measurements](#taking-measurements)!
 
-For retrieving G2G delay values from the Arduino, connect the measurement system to a computer via USB, and use [delayrecorder.py](https://github.com/cbachhuber/G2GDelay/blob/master/delayrecorder.py) to guide you through the measurement process.
-Even quicker: use the serial monitor of Arduino's IDE to retrieve G2G delay values.
+There are three main components in this repository: the circuit layout (file [circuit.svg](circuit.svg)) for the measurement device, the Arduino source code (in folder [Arduino_code](Arduino_code)) and a Python script for taking measurements and plotting them with statistics (file [delayrecorder.py](delayrecorder.py)). The quickest way to view measurements is to read the values directly from the Arduino IDE's Serial Monitor, and the most convenient is to run the script.
 
 ## Construction Manual
 
-For building the measurement device, you need the following parts
-
+For building the measurement device, you need the following parts:
 - Arduino Mega 2560: Does not have to be original Arduino, can also be e.g. a SunFounder Mega 2560
 - LED: A Light-emitting diode, e.g. LED 5-4500 RT
 - Phototransistor: For example the SDP 8406-003
 - Resistor 11kOhm: Any 11kOhm resistor will do the job, e.g. the 1/4W 11K
-- Cables
+- Wires
 - Optional: A breadboard
 
 Next, connect the elements as such:
@@ -25,33 +22,48 @@ Next, connect the elements as such:
   <img width="960" src="./circuit.svg">
 </p>
 
-
 You can simplify the above circuit and waive for example the breadboard.
 Make sure that pin A0 is not connected to anything (or pick a new analog pin for it) - it needs to be floating for the randomness to work properly.
 
-## Arduino source code
+## Software Setup
 
-[Download the Arduino IDE](https://www.arduino.cc/en/Main/Software), open the Arduino_code project.
-From the IDE, you can compile the code and then upload the binary to the Arduino board via USB.
+### Arduino Source Code
+To program the device, download and install the [Arduino IDE](https://www.arduino.cc/en/Main/Software). From the IDE, you can compile the code and then upload the binary to the Arduino board via USB.
 
-## Measuring with Computer
+### Python Script
+To be able to view the measurements using the provided script, install `python3` and ensure you have python modules `pyserial`, `matplotlib`, and `numpy`.
 
-The quickest way to get measurements is connecting the arduino to a computer using USB and observe the output on the serial console that comes with the Arduino IDE, or, more convenient, use the script delayrecorder.py to observe, process and store your measurements. Once the Arduino is connected to the USB port of a computer running the serial monitor of the arduino IDE, continue reading at [Starting Measurements](https://github.com/cbachhuber/G2GDelay#starting-measurements).
+## Hardware Setup
 
-## Starting Measurements
+### Powering the Device
+Connect the Arduino to the USB port of a computer.
 
-I strongly recommend to align the phototransistor (PT) directly to the LED in the beginning. The PT has a little bump on one side, this should point towards the LED and be very close to it and even touch the LED. This way, you can check whether the PT is correctly connected to the circuit. You should see samples with a delay of 0.00 milliseconds. This makes sense because there is nothing delaying the propagation of light between the LED and PT. 
+### Phototransistor Placement
+I strongly recommend to align the Phototransistor (PT) directly to the LED in the beginning. The PT has a little bump on one side, this should point towards the LED and be very close to it and even touch the LED. This way, you can check whether the PT is correctly connected to the circuit. You should see samples with a delay of 0.00 milliseconds. This makes sense because there is nothing delaying the propagation of light between the LED and PT
 
-Next, for testing the G2G latency of a video transmission system (e.g. your smartphone, with the camera application started), you can put the LED in the field of view of a camera and put the PT on the corresponding display where the LED is shown. Make sure to place the PT on the LED and let the knob on the PT face towards the screen. 
+### LED Placement
+Next, for testing the G2G latency of a video transmission system (e.g. your smartphone, with the camera application started), you can put the LED in the field of view of a camera and put the PT on the corresponding display where the LED is shown. Make sure to place the PT on the LED and let the bump on the PT face towards the screen.
+
+## Taking Measurements
+
+Once a program establishes communication with the Arduino, the LED should light up twice in the beginning, signalling that the Arduino started without an error. If it doesn't, try to flip the polarity of the LED contacts.
+
+### Arduino IDE
+This is the quickest way to get measurements. Run the Arduino IDE and open the Serial Monitor, you will be able to directly view the delay measurements.
+
+### Python Script
+This is a convenient way to observe and interpret measurements. [delayrecorder.py](delayrecorder.py) will take the desired filename and number of measurements, and it will generate a histogram with statistics and save the results. You can call it with `./delayrecorder.py -h` for more usage details and other options.
 
 ## Troubleshooting
 
-If you do not see any samples coming in, first try to adjust the position of the Phototransistor relative to the LED, if that does not succeed, try to flip the polarity of the Phototransistor contacts. Additional hints, if the system is working when putting the LED directly to the PT, but not when putting the LED on the screen:
+If you do not see any samples coming in, first try to bring the phototransistor (PT) closer to the LED, and align it more carefully. If that does not succeed, try to flip the polarity of the PT contacts.
 
+If the system is working when putting the LED directly up to the PT, but not when putting the LED on the screen, here are some additional strategies:
 - The system is based on detecting a brightness increase at the PT. Therefore, maximize the screen brightness to minimize the influence of ambient light.
 - Make sure that the environment of the turned off LED is depicted as dark as possible on the screen, and that the enabled LED is sufficiently bright.
 - You might want to attach the PT with an adhesive film strip to the monitor, if you tend to lose the location of the LED while manually holding the PT to the screen.
 
+## Conclusion
 
 Congratulations, you are now all set to do your very own Glass-to-Glass delay measurements! If you use the system in course of your research, please reference our corresponding paper ["A System for High Precision Glass-to-Glass Delay Measurements in Video Communication"](https://doi.org/10.1109/ICIP.2016.7532735). Thank you very much!
 
@@ -62,6 +74,5 @@ Congratulations, you are now all set to do your very own Glass-to-Glass delay me
       pages={2132--2136},
       year={2016},
     }
-
 
 Further details about the measurement system are provided on [arXiv](https://arxiv.org/abs/1510.01134v1). Feel free to contact me (christoph dot bachhuber at tum dot de) in case you run into any difficulties.
--- delayrecorder.py
+++ delayrecorder.py
@@ -1,87 +1,196 @@
-import serial
-import serial.tools.list_ports
+#!/usr/bin/env python3
+
 import sys
+import argparse
+import signal
 import time
-#import pyperclip
+import serial, serial.tools.list_ports
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+from dataclasses import dataclass
 
-errormsg = "Call this function using \n\n'python recorder.py [number of measurements] [path to savefile]'\n\nwhere the parameters are optional. E.g. 'python delayrecorder.py 50 stats.csv' takes 50 G2G delay measurements and writes them to file 'stats.csv'.\nBy default (no parameters given, i.e. 'python delayrecorder.py'), the script records 100 samples and does not create a csv file."
+@dataclass
+class Stats:
+    num_measurements: int
+    min_delay: float
+    max_delay: float
+    mean_delay: float
+    median_delay: float
+    std_dev: float
 
-args = sys.argv
-if len(args)==1:
-	iterations = 100
-elif len(args) ==2:
-	try:
-		iterations = int(args[1])
-	except:
-		print errormsg
-		quit()
-elif len(args) == 3:
-	try:
-		iterations = int(args[1])
-		savefile = args[2]
-	except:
-		print errormsg
-		sys.exit()
-else:
-	print errormsg
-	sys.exit()
+def sigint_handler(signal, frame):
+    print("Caught KeyboardInterrupt, exiting.")
+    sys.exit(0)
 
-ports = list(serial.tools.list_ports.comports())
-found = 0
-for p in ports:
-	if "Arduino" in p[1]:
-		print "Found Arduino at " + p[0]
-		ser = serial.Serial(p[0], 115200,timeout=5)
-		found = 1
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="This script obtains delay measurements from a Glass-to-Glass device connected via USB, saves results and statistics to a CSV file, and displays the results in a histogram plot. You can also use it to display results from saved measurements in a previous test, in which case the CSV file is read from rather than written to.")
 
-if found==0:
-	print "Did not find Arduino on any serial port. Is it connected?"
-	quit()
+    parser.add_argument('filename', nargs='?', default="results.csv", type=Path, help="The name of the CSV file where the data is saved. It is also the name for the saved plot. Default is 'results.csv' which will cause 'results.png' to be created as well. This is the filename used when using the '--readcsv' option as well.")
 
-timeout = time.time() + 0.01
-while True:
-	a = ser.readline()
-	if time.time() > timeout:
-		break
+    parser.add_argument('num_measurements', nargs='?', default=100, type=int, help="An integer for the number of measurements to save and use when generating statistics. Default is 100.")
 
-G2Gdelays = []
+    parser.add_argument('--quiet', '-q', action='store_true', help="The script won't print the measurements to the terminal (but will still save them into the CSV file).")
 
-i=0
-overallrounds = 0
-initmessage = 0
+    parser.add_argument('--readcsv', '-r', action='store_true', help="Reads a previously generated CSV and plots it. Be sure to provide the name of the CSV file if it's not the default name.")
 
-try:
-	while (i < iterations):
-		overallrounds = overallrounds + 1
-		a = ser.readline()
-		if "." in a:
-			initmessage = 1
-			i = i + 1
-			a = a.replace("\n","")
-			a = a.replace("\r","")
-			G2Gdelays.append(float(a))
-			print "G2G Delay: " + a + " ms"
-		else:
-			if overallrounds > 0 and initmessage==1:
-				print "Did not receive msmt data from the Arduino for another 5 seconds. Is the phototransistor still sensing the LED?"
-			else:
-				print "Did not receive msmt data from the Arduino for 5 seconds. \n Is the phototransistor sensing the LED on the screen? \n Is the correct side of the PT pointing towards the screen (the flat side with the knob on it)? \n Is the screen brightness high enough (max recommended)?"
-				initmessage = 1
-except KeyboardInterrupt:
-	pass
+    return parser.parse_args()
 
-# Post-processing
-time.sleep(.1)
-print "\n"
-print "Min/Mean/Max G2G Delay: " + '%.2f' % (min(G2Gdelays)) + "/" + '%.2f' % (sum(G2Gdelays)/float(len(G2Gdelays))) + "/" + '%.2f' % (max(G2Gdelays)) + " ms"
+def find_arduino_on_serial_port():
+    devs = serial.tools.list_ports.comports()
+    found = 0
+    for dev in devs:
+        if dev.manufacturer is not None:
+            if "Arduino" in dev.manufacturer:
+                print(f"Found Arduino at {dev[0]}")
+                ser = serial.Serial(dev[0], 115200,timeout=5)
+                found = 1
+    if found == 0:
+        print("Did not find Arduino on any serial port. Is it connected?")
+        sys.exit()
 
-#Copying to clipboard
-#pyperclip.copy('%.2f' % (min(G2Gdelays)) + "/" + '%.2f' % (sum(G2Gdelays)/float(len(G2Gdelays))) + "/" + '%.2f' % (max(G2Gdelays)) + " ms")
-#spam=pyperclip.paste()
+    return ser
 
-#Saving to csv file
-if 'savefile' in globals():
-	with open(savefile, 'wb') as f:
-		writer = csv.writer(f, delimiter = ',')
-		writer.writerow(G2Gdelays)
+def get_measurements_serial(num_measurements, quiet_mode):
+    ser = find_arduino_on_serial_port()
+
+    print(f"Collecting {num_measurements} measurements from the Arduino")
+    if quiet_mode:
+        print("Running in quiet mode, won't print the measurements to the terminal")
+
+    # Read messages from Arduino
+    timeout = time.time() + 0.01
+    while True:
+        a = ser.readline().decode()
+        if time.time() > timeout:
+            break
+
+    measurements = []
+    i = 0
+    overall_rounds = 0
+    init_message = 0
+
+    while (i < num_measurements):
+        overall_rounds += 1
+        a = ser.readline().decode()
+        if "." in a:
+            init_message = 1
+            i += 1
+            a = a.replace("\n","")
+            a = a.replace("\r","")
+            measurements.append(float(a))
+            if not quiet_mode:
+                print(f"G2G Delay trial {i}/{num_measurements}: {a} ms")
+            else:
+                print(f"G2G Delay trial {i}/{num_measurements}", end="\r")
+        else:
+            if overall_rounds > 0 and init_message == 1:
+                print("Did not receive msmt data from the Arduino for another 5 seconds. Is the phototransistor still sensing the LED?")
+            else:
+                print("Did not receive msmt data from the Arduino for 5 seconds. \n Is the phototransistor sensing the LED on the screen? \n Is the correct side of the PT pointing towards the screen (the flat side with the knob on it)? \n Is the screen brightness high enough (max recommended)?")
+                init_message = 1
+
+    return measurements
+
+def write_measurements_csv(filename, measurements, stats):
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+
+        header = ['Samples', 'Min', 'Max', 'Mean', 'Median', 'stdDev']
+        writer.writerow(header)
+
+        stats_list = [stats.num_measurements, stats.min_delay, stats.max_delay, stats.mean_delay, stats.median_delay, stats.std_dev]
+        writer.writerow(stats_list)
+
+        writer.writerow(measurements)
+
+    print(f"Saved results to {filename}")
+
+def get_measurements_csv(filename):
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        # 2nd row has the stats, 3rd row has the datapoints
+        for i, row in enumerate(reader):
+            if i == 1:
+                # convert every string in the array into a float
+                stats_list = [float(i) for i in row]
+                # grab each element of the list to create the Stats object
+                stats = Stats(*stats_list)
+            if i == 2:
+                measurements = [float(i) for i in row]
+    print(f"Obtained values from {filename}")
+
+    return measurements, stats
+
+def generate_stats(measurements):
+    measurements_np = np.array(measurements)
+
+    min_delay = np.min(measurements_np)
+    max_delay = np.max(measurements_np)
+    mean_delay = np.mean(measurements_np)
+    median_delay = np.median(measurements_np)
+    std_dev = np.std(measurements_np)
+
+    stats = Stats(len(measurements_np), min_delay, max_delay, mean_delay, median_delay, std_dev)
+
+    print(f"\nmin: {min_delay:.2f} ms | max: {max_delay:.2f} ms | median: {median_delay:.2f} ms")
+    print(f"mean: {mean_delay:.2f} ms | std_dev: {std_dev:.2f} ms\n")
+
+    return measurements_np, stats
+
+def plot_results(measurements, stats, fig_name):
+    plt.hist(measurements, bins=20)
+    plt.gcf().canvas.manager.set_window_title(fig_name)
+    plt.title('Latency Histogram')
+    plt.xlabel('Latency (ms)')
+    plt.ylabel('Frequency')
+
+    ax = plt.gca()
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    textstr1 = '\n'.join((
+        r'$\mathrm{min}=%.2f$' % (stats.min_delay,),
+        r'$\mathrm{max}=%.2f$' % (stats.max_delay,),
+        r'$\mathrm{median}=%.2f$' % (stats.median_delay,)))
+    # place it at position x=0.05, y=0.95, relative to the top and left of the box
+    ax.text(0.05, 0.95, textstr1, transform=ax.transAxes, fontsize=14,
+        verticalalignment='top', horizontalalignment='left', bbox=props)
+    textstr2 = '\n'.join((
+        r'$\mu=%.2f$' % (stats.mean_delay, ),
+        r'$\sigma=%.2f$' % (stats.std_dev,)))
+    # place it at position x=0.95, y=0.90, relative to the top and right of the box
+    ax.text(0.95, 0.95, textstr2, transform=ax.transAxes, fontsize=14,
+        verticalalignment='top', horizontalalignment='right', bbox=props)
+
+    plt.savefig(fig_name)
+    print(f"Saved histogram to {fig_name}")
+    plt.show()
+
+def main():
+    # Set up signal handler to handle keyboard interrupts
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    args = parse_arguments() # parse command line input arguments
+    file_extension = args.filename.suffix
+    if file_extension != '.csv':
+        print("Error: Provided filename is invalid or does not have .csv extension")
+        sys.exit()
+
+    # Either write the data to a CSV file or read from a preexisting one
+    fig_name = args.filename.with_suffix('.png').name
+    if not args.readcsv:
+        g2g_delays = get_measurements_serial(args.num_measurements, args.quiet)
+        time.sleep(.1)
+        # this function returns g2g_delays as a numpy array
+        g2g_delays, stats = generate_stats(g2g_delays)
+        write_measurements_csv(args.filename, g2g_delays, stats)
+    else:
+        print(f"Reading data from {args.filename}")
+        # this function returns g2g_delays as a list
+        g2g_delays, stats = get_measurements_csv(args.filename)
+
+    # save plot to a png file and display it. Data type doesn't seem to matter
+    plot_results(g2g_delays, stats, fig_name)
+
+if __name__ == "__main__":
+    main()
